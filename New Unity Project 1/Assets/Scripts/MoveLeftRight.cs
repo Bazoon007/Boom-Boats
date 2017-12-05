a2d1e1a41704b4f7ad61f430b5f7f959e69e5f1c
ndanieli <ndanieli@gmail.com>
2017-12-05 08:39:30 +0200

    Cannons, Background, Landscape
    Set the camera to landscape.
    Made a moving water background.
    Made moving cannons that can shoot

---
+++ New Unity Project 1/Assets/Scripts/MoveLeftRight.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveLeftRight : MonoBehaviour {
+    public float rotationSpeed;
+    public float rotationLimit;
+    public Transform shootingPointTranform;
+    public int location;
+
+    private float startingXRotation;
+    private float startingYRotation;
+
+    // Use this for initialization
+    void Start () {
+        startingXRotation = transform.rotation.x;
+        startingYRotation = transform.rotation.y;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(new Vector3(0f, 1f, 0f), rotationSpeed * Time.deltaTime);
+        
+        if (location == 1)
+        {
+            if (rotationSpeed > 0)
+            {
+                if (transform.rotation.x > startingXRotation + rotationLimit)
+                {
+                    rotationSpeed *= -1;
+                }
+            }
+            else
+            {
+                if (transform.rotation.x < startingXRotation - rotationLimit)
+                {
+                    rotationSpeed *= -1;
+                }
+            }
+        }
+        else if (location == 2)
+        {
+            if (rotationSpeed > 0)
+            {
+                if (transform.rotation.y < startingYRotation - rotationLimit)
+                {
+                    rotationSpeed *= -1;
+                }
+            }
+            else
+            {
+                if (transform.rotation.y > startingYRotation + rotationLimit)
+                {
+                    rotationSpeed *= -1;
+                }
+            }
+            //Debug.Log(transform.rotation.y);
+        }
+        
+            //Debug.Log(startingXRotation /*+ " " + transform.rotation.x*/ + " " + location);
+	}
+}
