c75629b96a17a8741c523820c97f7eb95b52e56d
ndanieli <ndanieli@gmail.com>
2017-12-27 11:04:55 +0200

    Added flag behavior for hits
    remove a flag when hit,
    code for adding a flag is ready (power up), next step is to call it at the end of a wave

--- Boom Boats/Assets/Scripts/Island Scripts/IslandHealth.cs
+++ Boom Boats/Assets/Scripts/Island Scripts/IslandHealth.cs
@@ -7,25 +7,59 @@ public class IslandHealth : MonoBehaviour {
     private int health;
     public int initialHealth;
     public MasterManager masterManager;
+    public GameObject OneHealthRemainingFlag;
+    public GameObject TwoHealthRemainingFlag;
+    public GameObject ThreeHealthRemainingFlag;
+
     
     private void Start()
     {
         health = initialHealth;
+        OneHealthRemainingFlag.SetActive(true);
+        TwoHealthRemainingFlag.SetActive(true);
+        ThreeHealthRemainingFlag.SetActive(true);
     }
 
     public void ReduceIslandHealth()
     {
         //Debug.Log("Island was hit");
         health--;
-        if (health == 0)
+
+        if (health == 2)
+        {
+            ThreeHealthRemainingFlag.SetActive(false);
+        }
+        else if (health == 1)
+        {
+            TwoHealthRemainingFlag.SetActive(false);
+        }
+        else if (health == 0)
         {
             islandDied();
+        }
+        
+    }
 
+    private void IncreaseIslandHealth()
+    {
+        if (health < 3)
+        {
+            health++;
+        }
+
+        if (health == 2)
+        {
+            TwoHealthRemainingFlag.SetActive(true);
+        }
+        else if (health == 3)
+        {
+            ThreeHealthRemainingFlag.SetActive(true);
         }
     }
 
     private void islandDied()
     {
+        OneHealthRemainingFlag.SetActive(false);
         masterManager.waveManager.IncreaseWave(true);
         masterManager.spawnManager.OnIslandDeath(gameObject.GetComponent<IslandCannonRelation>().cannon.location);
         gameObject.GetComponent<IslandCannonRelation>().cannon.gameObject.SetActive(false);
@@ -37,5 +71,8 @@ public class IslandHealth : MonoBehaviour {
         health = initialHealth;
         gameObject.GetComponent<IslandCannonRelation>().cannon.gameObject.SetActive(true);
         gameObject.SetActive(true);
+        OneHealthRemainingFlag.SetActive(true);
+        TwoHealthRemainingFlag.SetActive(true);
+        ThreeHealthRemainingFlag.SetActive(true);
     }
 }
